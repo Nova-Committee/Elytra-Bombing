9b5da1c08801efdddc9f03ed94f730df5d1a227d
Tapio <mikhailtapio13468@163.com>
2022-02-08 23:08:39 +0800

    Initialization && Release

---
+++ src/main/java/committee/nova/elytraBombing/common/data/tag/Tags.java
@@ -0,0 +1,10 @@
+package committee.nova.elytraBombing.common.data.tag;
+
+import net.minecraft.tags.Tag;
+import net.minecraft.world.item.Item;
+
+import static net.minecraft.tags.ItemTags.bind;
+
+public class Tags {
+    public static final Tag.Named<Item> IGNITERS = bind("igniters");
+}
