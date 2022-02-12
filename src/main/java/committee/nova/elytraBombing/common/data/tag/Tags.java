e953b4132c269c8f3fc50b1bf2557a2477824db5
Tapio <mikhailtapio13468@163.com>
2022-02-12 10:46:52 +0800

    Port to 1.15.2

---
+++ src/main/java/committee/nova/elytraBombing/common/data/tag/Tags.java
@@ -0,0 +1,10 @@
+package committee.nova.elytraBombing.common.data.tag;
+
+import net.minecraft.item.Item;
+import net.minecraft.tags.ItemTags;
+import net.minecraft.tags.Tag;
+import net.minecraft.util.ResourceLocation;
+
+public class Tags {
+    public static final Tag<Item> IGNITERS = new ItemTags.Wrapper(new ResourceLocation("igniters"));
+}
