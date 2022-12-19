e32586fde1b2b7e20d61786aed9f6ad7fc69ae60
MikhailTapio <mikhailtapio13468@163.com>
2022-12-19 10:29:01 +0800

    Port to Forge 1.19.2

---
+++ src/main/java/committee/nova/elytraBombing/common/data/tag/Tags.java
@@ -0,0 +1,11 @@
+package committee.nova.elytraBombing.common.data.tag;
+
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.TagKey;
+import net.minecraft.world.item.Item;
+
+import static net.minecraft.tags.ItemTags.create;
+
+public class Tags {
+    public static final TagKey<Item> IGNITERS = create(new ResourceLocation("igniters"));
+}
---
+++ src/main/java/committee/nova/elytraBombing/common/event/BombingListener.java
@@ -0,0 +1,40 @@
+package committee.nova.elytraBombing.common.event;
+
+import committee.nova.elytraBombing.common.config.CommonConfig;
+import committee.nova.elytraBombing.common.data.tag.Tags;
+import committee.nova.elytraBombing.common.tools.player.PlayerHandler;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.Items;
+import net.minecraftforge.event.entity.player.PlayerInteractEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+import java.text.MessageFormat;
+
+@Mod.EventBusSubscriber
+public class BombingListener {
+    @SubscribeEvent
+    public static void onPlayerRightClickEvent(final PlayerInteractEvent.RightClickItem event) {
+        final var player = event.getEntity();
+        if (!player.isFallFlying()) return;
+        final var igniter = event.getItemStack();
+        if (!igniter.is(Tags.IGNITERS)) return;
+        final var tnt = PlayerHandler.searchFor(player, Items.TNT, 0);
+        if (tnt == null || tnt.isEmpty()) return;
+        final var tntItem = tnt.getItem();
+        if (player.getCooldowns().isOnCooldown(tntItem)) {
+            final int cd = (int) (player.getCooldowns().getCooldownPercent(tntItem, 0F) * CommonConfig.TNT_BOMBING_CD.get());
+            final var isPlural = cd > 1;
+            if (!player.level.isClientSide) {
+                player.displayClientMessage(
+                        Component.translatable(MessageFormat.format(
+                                Component.translatable("msg.ebb.bombing_cd").getString(),
+                                cd,
+                                isPlural ? Component.translatable("msg.ebb.unit.plural_suffix").getString() : ""
+                        )), true);
+            }
+            return;
+        }
+        PlayerHandler.launchTnt(player, igniter, tnt);
+    }
+}
